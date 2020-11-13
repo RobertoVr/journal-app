aad30957cb94359a0ecfa29c5b0f01d60cf0ce06
Roberto Valdez <robert.tito94@gmail.com>
2020-11-13 12:57:29 -0600

    sweet alert

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,3 +1,5 @@
+import Swal from 'sweetalert2'
+
 import { firebase, googleAuthProvider } from '../firebase/firebase-config'
 import { types } from "../types/types"
 import { finishLoading, startLoading } from './ui';
@@ -12,6 +14,7 @@ export const startLoginEmailPassword = (email, password) => {
             })
             .catch(e => {
                 dispatch(finishLoading());
+                Swal.fire('Error', e.message, 'error');
             });
     };
 }
@@ -29,6 +32,7 @@ export const startRegisterWithEmailPasswordName = (email, password, name) => {
             })
             .catch(e => {
                 dispatch(finishLoading());
+                Swal.fire('Error', e.message, 'error');
             });
     }
 }
