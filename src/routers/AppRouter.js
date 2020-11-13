efc6ce7d6abb3f1911e0e1b15f8dfb4d3db963e1
Roberto Valdez <robert.tito94@gmail.com>
2020-11-13 12:03:09 -0600

    mantener sesión

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
+import { useDispatch } from 'react-redux';
+import { firebase } from '../firebase/firebase-config';
 import { JournalScreen } from '../components/journal/JournalScreen';
 import { AuthRouter } from './AuthRouter';
+import { login } from '../actions/auth';
 
 export const AppRouter = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user?.uid) {
+                dispatch(login(user.uid, user.displayName));
+            }
+        });
+    }, [dispatch]);
+
     return (
         <Router>
             <div>
